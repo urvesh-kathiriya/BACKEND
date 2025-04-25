e68cadfe306276a53310611bcb1a9bd232d3c0b6
urvesh-kathiriya <urveshkathiriya@gmail.com>
2025-04-25 11:58:46 +0530

    set cloudinary config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,16 +1,9 @@
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from"../config/cloudinary.config.js";
 import fs from "fs";
 
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        console.log(localFilePath)
         if (!localFilePath) return null
         //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
