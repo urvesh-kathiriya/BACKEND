0c436bcbd4c349f751f6c448f12fbe02d5fdc39e
urvesh-kathiriya <urveshkathiriya@gmail.com>
2025-05-02 15:22:33 +0530

    Add routes for comments, health check, likes, playlists, subscriptions, tweets, and videos

--- src/app.js
+++ src/app.js
@@ -18,8 +18,22 @@ app.use(cookieParser());
 const api=process.env.API_URL ;
 
 import UserRouter from './routes/user.route.js';
+import CommentRoute from './routes/comment.route.js';
+import HealthCheckRoute from './routes/healthcheck.route.js';
+import LikeRoute from './routes/like.route.js';
+import PlayListRoute from './routes/playlist.route.js';
+import SubscriptionRoute from './routes/subscription.route.js';
+import TweetRoute from './routes/tweet.route.js';
+import VideoRoute from './routes/video.route.js';
 
 
 app.use(`${api}/user`,UserRouter)
+app.use(`${api}/comment`,CommentRoute)
+app.use(`${api}/healthcheck`,HealthCheckRoute)
+app.use(`${api}/like`,LikeRoute)
+app.use(`${api}/playlist`,PlayListRoute)
+app.use(`${api}/subscription`,SubscriptionRoute)
+app.use(`${api}/tweet`,TweetRoute)
+app.use(`${api}/video`,VideoRoute)
 
 export default app;
